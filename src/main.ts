2ef3f6a56b6486f05e0a8b294ed8b744cb3d7bc8
Ahmed <ahmedelsharawy117@gmail.com>
2024-06-17 12:02:26 +0300

    added logger middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import express, { Express, Request, Response } from "express";
+import { logger } from "./middleware/logger";
 
 const app: Express = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(logger);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
---
+++ src/middleware/logger.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+
+export function logger(req: Request, res: Response, next: NextFunction) {
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const userAgent = req.headers["user-agent"];
+  const time = new Date().toISOString();
+
+  console.log(
+    `${req.method} ${req.originalUrl} - ${ip} (${userAgent}) - ${time}`,
+    "\n",
+  );
+
+  next();
+}
