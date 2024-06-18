192b1cdd6a0ca0ba6f9a445b5e06a4a77276988c
Ahmed <ahmedelsharawy117@gmail.com>
2024-06-18 18:44:20 +0300

    modified logger

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,14 +1,17 @@
 import { Request, Response, NextFunction } from "express";
+import asyncHandler from "express-async-handler";
 
-export function logger(req: Request, res: Response, next: NextFunction) {
-  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-  const userAgent = req.headers["user-agent"];
-  const time = new Date().toISOString();
+export const logger = asyncHandler(
+  (req: Request, res: Response, next: NextFunction) => {
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    const userAgent = req.headers["user-agent"];
+    const time = new Date().toISOString();
 
-  console.log(
-    `${req.method} ${req.originalUrl} - ${ip} (${userAgent}) - ${time}`,
-    "\n",
-  );
+    console.log(
+      `${req.method} ${req.originalUrl} - ${ip} (${userAgent}) - ${time}`,
+      "\n",
+    );
 
-  next();
-}
+    next();
+  },
+);
