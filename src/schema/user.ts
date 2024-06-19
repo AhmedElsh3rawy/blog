89d5cd9e2404f26a43554383d3098e916d90c78a
Ahmed <ahmedelsharawy117@gmail.com>
2024-06-20 02:29:03 +0300

    some changes

--- src/schema/user.ts
+++ src/schema/user.ts
@@ -1,6 +1,6 @@
 import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
-export const user = pgTable("user", {
+export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   username: text("username").notNull(),
   email: text("email").notNull(),
