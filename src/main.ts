dba8500c2d4652b5587e3284bdc181b704477bd6
Ahmed <ahmedelsharawy117@gmail.com>
2024-06-23 23:26:24 +0300

    added some middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import { logger } from "./middleware/logger";
 import authRoutes from "./routes/authRoutes";
+
 const app: Express = express();
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(logger);
+app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", authRoutes);
 
 app.get("/", (req: Request, res: Response) => {
