5e0a62cf7cdbf9548c9c4c8ca50f3d6e79d6d1f4
Ahmed <ahmedelsharawy117@gmail.com>
2024-06-23 09:26:04 +0300

    modified

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import express, { Express, Request, Response } from "express";
 import { logger } from "./middleware/logger";
-
+import authRoutes from "./routes/authRoutes";
 const app: Express = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.json());
 app.use(logger);
+app.use("/api/users", authRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Hello, World! ʕ ·(エ)· ʔ");
