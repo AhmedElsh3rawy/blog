62e57fcbe2405b467db38b4e3607b65ad9a39ec9
Ahmed <ahmedelsharawy117@gmail.com>
2024-06-23 15:12:22 +0300

    almost fienshed

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { findUser, addUser } from "../database/queries";
 import { hashPassword, comparePasswords } from "../utils/password";
 import { response } from "../utils/responseHelper";
+import { generateAccessToken, generateRefreshToken } from "../utils/jwtToken";
 
 export const register = async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
@@ -24,13 +25,26 @@ export const login = async (req: Request, res: Response) => {
   }
   const user = await findUser(email);
   if (!user) {
-    return res.json(response("Not registered", 400));
+    return res.json(response("You are not registered", 400));
   }
   const match = comparePasswords(user.password, password);
   if (!match) {
     return res.json(response("Invalid email or password", 400));
   }
-  // TODO
+  const accessToken = await generateAccessToken(user.id);
+  const refreshToken = await generateRefreshToken(user.id);
+  res.cookie("refreshToken", refreshToken, {
+    maxAge: 90 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  });
+  res.status(200).json({ accessToken });
 };
 
-export const logout = async (req: Request, res: Response) => { };
+export const logout = async (req: Request, res: Response) => {
+  const refreshToken = req?.cookies?.refreshToken;
+  if (!refreshToken) return res.json(response("You are not logged in", 403));
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+  });
+  res.status(200).json(response("Logged out successfuly", 200));
+};
