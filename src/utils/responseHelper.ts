409e5b7cbeb3e62d99b977d4578070f2fcc874d9
Ahmed <ahmedelsharawy117@gmail.com>
2024-06-23 09:29:15 +0300

    modified resHelper

--- src/utils/responseHelper.ts
+++ src/utils/responseHelper.ts
@@ -1,12 +1,4 @@
-export function ok<T = any>(message: string, data: T, created = false) {
-  return {
-    message,
-    statusCode: created ? 201 : 200,
-    data,
-  };
-}
-
-export const res = (message: string, statusCode: number) => {
+export const response = (message: string, statusCode: number) => {
   return {
     message,
     statusCode,
