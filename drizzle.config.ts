1f5f82f38c6c9e270668b488f4e002701c9a73ac
Ahmed <ahmedelsharawy117@gmail.com>
2024-06-24 05:49:41 +0300

    modified path to schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./src/schema/*",
+  schema: "./src/database/schema.ts",
   out: "./drizzle/migrations",
   dbCredentials: {
     url: process.env.DB_URL as string,
