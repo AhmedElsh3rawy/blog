a55ae967eaab3bad644385b06ccc58fdd5d60e08
Ahmed <ahmedelsharawy117@gmail.com>
2024-06-29 21:26:19 +0300

    modified appError

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -1,8 +1,17 @@
 class AppError extends Error {
   statusCode: number;
+  statusText: string;
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
+    this.statusText =
+      statusCode == 401
+        ? "Unauthorized"
+        : statusCode == 403
+          ? "Forbidden"
+          : statusCode == 404
+            ? "Not Found"
+            : "Internal Server Error";
   }
 }
 
