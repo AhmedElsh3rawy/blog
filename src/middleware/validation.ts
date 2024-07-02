0bc5c670fd9b0cbaa5c806d846bdb129f7841d0b
Ahmed <ahmedelsharawy117@gmail.com>
2024-07-02 08:03:23 +0300

    validation middleware

---
+++ src/middleware/validation.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { validationResult, Result } from "express-validator";
+
+export const validation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const errors: Result = validationResult(req);
+  if (!errors.isEmpty()) {
+    const formattedErrors = errors.array().map((error) => ({
+      field: error.param,
+      message: error.msg,
+    }));
+    return res.status(400).json({
+      message: "Validation faild",
+      errors: formattedErrors,
+      statusCode: 400,
+    });
+  }
+  next();
+};
