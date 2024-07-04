e018b7e94d8f2039c14e5ae23e3ae3be703a4750
Ahmed <ahmedelsharawy117@gmail.com>
2024-07-04 08:22:27 +0300

    use async handler

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,17 +1,14 @@
 import { Request, Response, NextFunction } from "express";
-import asyncHandler from "express-async-handler";
 
-export const logger = asyncHandler(
-  (req: Request, res: Response, next: NextFunction) => {
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    const userAgent = req.headers["user-agent"];
-    const time = new Date().toISOString();
+export const logger = (req: Request, res: Response, next: NextFunction) => {
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const userAgent = req.headers["user-agent"];
+  const time = new Date().toISOString();
 
-    console.log(
-      `${req.method} ${req.originalUrl} - ${ip} (${userAgent}) - ${time}`,
-      "\n",
-    );
+  console.log(
+    `${req.method} ${req.originalUrl} - ${ip} (${userAgent}) - ${time}`,
+    "\n",
+  );
 
-    next();
-  },
-);
+  next();
+};
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,22 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+import { asyncHandler } from "../utils/asyncHandler";
 import { validationResult, Result } from "express-validator";
 
-export const validation = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const errors: Result = validationResult(req);
-  if (!errors.isEmpty()) {
-    const formattedErrors = errors.array().map((error) => ({
-      field: error.param,
-      message: error.msg,
-    }));
-    return res.status(400).json({
-      message: "Validation faild",
-      errors: formattedErrors,
-      statusCode: 400,
-    });
-  }
-  next();
-};
+export const validation = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors: Result = validationResult(req);
+    if (!errors.isEmpty()) {
+      const formattedErrors = errors.array().map((error) => ({
+        field: error.param,
+        message: error.msg,
+      }));
+      return res.status(400).json({
+        message: "Validation faild",
+        errors: formattedErrors,
+        statusCode: 400,
+      });
+    }
+    next();
+  },
+);
