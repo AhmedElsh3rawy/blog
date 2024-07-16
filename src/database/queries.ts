4c30b45f0f490fc2d871e1efa71b06de3e95cbab
Ahmed <ahmedelsharawy117@gmail.com>
2024-07-16 14:44:19 +0300

    add post crud

--- src/database/queries.ts
+++ src/database/queries.ts
@@ -1,28 +1,47 @@
 import { db } from "./db";
 import { eq, sql } from "drizzle-orm";
-import { users, tokens } from "./schema";
+import { users, posts, tokens } from "./schema";
 
-export async function findUser(email: string) {
+export const findUser = async (email: string) => {
   const user = await db.query.users.findFirst({
     where: sql`${users.email} = ${email}`,
   });
   return user;
-}
+};
 
-export async function addUser(
+export const addUser = async (
   username: string,
   email: string,
   password: string,
-) {
+) => {
   await db
     .insert(users)
     .values({ username: username, email: email, password: password });
-}
+};
 
-export async function addToken(token: string, id: number) {
+export const addToken = async (token: string, id: number) => {
   await db.insert(tokens).values({ refreshToken: token, userId: id });
-}
+};
 
-export async function deleteToken(id: number) {
+export const deleteToken = async (id: number) => {
   await db.delete(tokens).where(eq(tokens.userId, id));
-}
+};
+
+export const getPost = async (postId: number) => {
+  return await db.select().from(posts).where(eq(posts.id, postId));
+};
+
+export const addPost = async (content: string, authorId: number) => {
+  await db.insert(posts).values({ content: content, authorId: authorId });
+};
+
+export const updatePost = async (postId: number, newContent: string) => {
+  await db
+    .update(posts)
+    .set({ content: newContent })
+    .where(eq(posts.id, postId));
+};
+
+export const deletePost = async (postId: number) => {
+  await db.delete(posts).where(eq(posts.id, postId));
+};
