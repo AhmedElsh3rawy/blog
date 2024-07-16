08df6c38d5fcf550202afb5fb955e26492698498
Ahmed <ahmedelsharawy117@gmail.com>
2024-07-16 11:19:22 +0300

    refactor: middlewer.auth

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,23 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { response } from "../utils/responseHelper";
+import appError from "../utils/appError";
 
-export function authenticateToken(
+export const authenticateToken = (
   req: Request,
   res: Response,
   next: NextFunction,
-) {
+) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (token == null) {
-    return res.status(401).json(response("Unauthoraized access", 401));
+  if (!token) {
+    return next(new appError("Access Denied. No token provided.", 401));
   }
 
-  const decoded = jwt.verify(
-    token,
-    process.env.ACCESS_TOKEN_SECRET as string,
-  ) as { id: string };
-  req.user = decoded;
-  next();
-}
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET as string,
+    ) as { id: string };
+    req.user = decoded;
+    next();
+  } catch (e) {
+    console.error("Error: ", e);
+    return next(new appError("Invalid token", 400));
+  }
+};
