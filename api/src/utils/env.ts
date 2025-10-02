93f238595e92c6d5c31fba1f6063adfb9f0d9490
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-02 23:01:46 +0300

    style: formatting

--- api/src/utils/env.ts
+++ api/src/utils/env.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-	PORT: z.coerce.number().gte(1000),
-	NODE_ENV: z
-		.union([z.literal("development"), z.literal("production")])
-		.default("development"),
+  PORT: z.coerce.number().gte(1000),
+  NODE_ENV: z
+    .union([z.literal("development"), z.literal("production")])
+    .default("development"),
 });
 
 const env = envSchema.parse(process.env);
