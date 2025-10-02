5cfda402e2623e7e5a3d2393cfffdba7bd789e21
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-02 15:08:53 +0300

    feat: env variables validation helper

---
+++ api/src/utils/env.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+	PORT: z.coerce.number().gte(1000),
+	NODE_ENV: z
+		.union([z.literal("development"), z.literal("production")])
+		.default("development"),
+});
+
+const env = envSchema.parse(process.env);
+
+export default env;
