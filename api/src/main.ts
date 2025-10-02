18c9d35af94b3c5c04e5ce993680307b4c703fae
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-02 23:01:53 +0300

    style: formatting

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,15 @@
-import express, { type Request, Response } from "express";
+import * as express from "express";
+import type { Request, Response } from "express";
 import env from "./utils/env";
 
 const app = express();
 
 const PORT = env.PORT;
 
-app.get("/", (req, res) => {
-	res.send("Hello, stranger...");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, stranger...");
 });
 
 app.listen(PORT, () => {
-	console.log(`[server]: Running on http://localhost:${PORT}`);
+  console.log(`[server]: Running on http://localhost:${PORT}`);
 });
