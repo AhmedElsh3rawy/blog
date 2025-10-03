75917956c55bb07bd6c21f6fb718a9480b698784
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-03 10:26:59 +0300

    chore: env cast the port from string to number

--- api/src/utils/env.ts
+++ api/src/utils/env.ts
@@ -8,7 +8,7 @@ const envSchema = z.object({
   POSTGRES_USER: z.string().min(2),
   POSTGRES_PASSWORD: z.string().min(8),
   POSTGRES_DB: z.string().min(2),
-  POSTGRES_PORT: z.number().gte(5432),
+  POSTGRES_PORT: z.coerce.number().gte(5432),
 });
 
 const env = envSchema.parse(process.env);
