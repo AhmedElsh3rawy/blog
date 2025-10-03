e9e588d332185991f59bfcdad63ef70a2251d8f9
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-04 02:45:31 +0300

    fix: env variables validation ensure none empty

--- api/src/utils/env.ts
+++ api/src/utils/env.ts
@@ -6,14 +6,14 @@ const envSchema = z.object({
     .union([z.literal("development"), z.literal("production")])
     .default("development"),
 
-  POSTGRES_USER: z.string().min(2),
-  POSTGRES_PASSWORD: z.string().min(8),
-  POSTGRES_DB: z.string().min(2),
+  POSTGRES_USER: z.string().nonempty(),
+  POSTGRES_PASSWORD: z.string().nonempty(),
+  POSTGRES_DB: z.string().nonempty(),
   POSTGRES_PORT: z.coerce.number().gte(5432),
 
-  GOOGLE_CLIENT_ID: z.string().min(30),
-  GOOGLE_CLIENT_SECRET: z.string().min(30),
-  GOOGLE_CALLBACK_URL: z.string().min(30),
+  GOOGLE_CLIENT_ID: z.string().nonempty(),
+  GOOGLE_CLIENT_SECRET: z.string().nonempty(),
+  GOOGLE_CALLBACK_URL: z.string().nonempty(),
 });
 
 const env = envSchema.parse(process.env);
