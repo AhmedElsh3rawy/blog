407290f460e63f693a2091044d79a57552674ae1
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-03 10:33:41 +0300

    feat: use error handler and not found middlewares

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,18 +1,21 @@
 import express from "express";
-import type { Request, Response, NextFunction } from "express";
+import type { Request, Response } from "express";
 import env from "./utils/env";
-import logger from "./config/logger";
 import { logging } from "./middleware/logging";
+import { errorHandler, notFound } from "./middleware/error-handler";
 
 const app = express();
 
-app.use(logging);
-
 const PORT = env.PORT;
+
+app.use(logging);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, stranger...");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`[server]: Running on http://localhost:${PORT}`);
