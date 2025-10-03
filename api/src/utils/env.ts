87c2906b1c0029a402e0bc0e8f0b01c9deefa047
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-03 05:27:31 +0300

    feat: add db env variables

--- api/src/utils/env.ts
+++ api/src/utils/env.ts
@@ -5,6 +5,10 @@ const envSchema = z.object({
   NODE_ENV: z
     .union([z.literal("development"), z.literal("production")])
     .default("development"),
+  POSTGRES_USER: z.string().min(2),
+  POSTGRES_PASSWORD: z.string().min(8),
+  POSTGRES_DB: z.string().min(2),
+  POSTGRES_PORT: z.number().gte(5432),
 });
 
 const env = envSchema.parse(process.env);
