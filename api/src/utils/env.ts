15f4d1401d8f2e59990f41f05eceaa16c9d89d47
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-03 15:38:00 +0300

    chore: add env variables for oauth2 by google

--- api/src/utils/env.ts
+++ api/src/utils/env.ts
@@ -5,10 +5,15 @@ const envSchema = z.object({
   NODE_ENV: z
     .union([z.literal("development"), z.literal("production")])
     .default("development"),
+
   POSTGRES_USER: z.string().min(2),
   POSTGRES_PASSWORD: z.string().min(8),
   POSTGRES_DB: z.string().min(2),
   POSTGRES_PORT: z.coerce.number().gte(5432),
+
+  GOOGLE_CLIENT_ID: z.string().min(30),
+  GOOGLE_CLIENT_SECRET: z.string().min(30),
+  GOOGLE_CALLBACK_URL: z.string().min(30),
 });
 
 const env = envSchema.parse(process.env);
