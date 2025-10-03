828e1982b8c9c50f743ac57b1ca4573164f0c9f7
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-03 10:26:02 +0300

    feat: add winston logger

---
+++ api/src/config/logger.ts
@@ -0,0 +1,24 @@
+import * as winston from "winston";
+import env from "../utils/env";
+
+const { combine, timestamp, printf, colorize } = winston.format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: env.NODE_ENV === "production" ? "info" : "debug",
+  format: combine(
+    colorize(),
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    logFormat,
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
+});
+
+export default logger;
