96b8129f0904008d272a24fc0eb31d41343439e9
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-03 15:53:52 +0300

    feat: swagger setup function

---
+++ api/src/utils/swagger.ts
@@ -0,0 +1,10 @@
+import swaggerUi from "swagger-ui-express";
+import type { Express } from "express";
+import YAML from "yamljs";
+import path from "node:path";
+
+const swaggerSpec = YAML.load(path.join(__dirname, "../docs/swagger.yaml"));
+
+export const setupSwaggerDocs = (app: Express) => {
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
