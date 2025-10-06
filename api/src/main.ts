e938feaf4050432fbe2c94ab633c5cdfb7ba2ff8
Ahmed <ahmedelsharawy117@gmail.com>
2025-10-06 11:41:24 +0300

    feat: setup app to use passport and session

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,6 +2,8 @@ import express from "express";
 import type { Request, Response } from "express";
 import env from "./utils/env";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import session from "express-session";
 import cors from "cors";
 import { logging } from "./middleware/logging";
 import { errorHandler, notFound } from "./middleware/error-handler";
@@ -12,6 +14,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser(env.COOKIE_SECRET));
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 1000 * 60 * 15, // 15min
+    },
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(
   cors({
     origin: env.APP_URL,
